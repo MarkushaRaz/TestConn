476369774488de2d646d12bbcded8d543a5537eb
Markusha <LegoAnimation2022@gmail.com>
2024-11-11 19:39:33 +0000

    asegegf

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 import socket
 
 server = socket.socket()  # создаём объект сокета сервера
-hostname = '0.0.0.0'
-# hostname = socket.gethostname()  # получаем имя хоста локальной машины
-print(hostname)
-port = 5000  # устанавливаем порт сервера
+print(server)
+# hostname = socket.AddressInfo
+hostname = socket.gethostname()  # получаем имя хоста локальной машины
+port = 65134  # устанавливаем порт сервера
 server.bind((hostname, port))  # привязываем сокет сервера к хосту и порту
 print(server)
 server.listen(5)  # начинаем прослушивание входящих подключений [2](https://metanit.com/python/network/1.2.php)
